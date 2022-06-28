6c607b8790f88d04e55fa45268cfdcb6e6700ff4
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-06-28 18:16:22 +0100

    add PersonController content

--- src/main/java/com/files_and_folders/files_and_folders/controllers/PersonController.java
+++ src/main/java/com/files_and_folders/files_and_folders/controllers/PersonController.java
@@ -1,4 +1,34 @@
 package com.files_and_folders.files_and_folders.controllers;
 
+import com.files_and_folders.files_and_folders.models.Person;
+import com.files_and_folders.files_and_folders.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class PersonController {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @GetMapping(value = "/persons")
+    public ResponseEntity<List<Person>> getAllPerson(){
+        return new ResponseEntity<>(personRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/persons/{id}")
+    public Optional<Person> getPerson(@PathVariable Long id){
+        return personRepository.findById(id);
+    }
+
+    @PostMapping(value = "/persons")
+    public ResponseEntity<Person> postPerson(@RequestBody Person person){
+        personRepository.save(person);
+        return new ResponseEntity<>(person, HttpStatus.CREATED);
+    }
 }
