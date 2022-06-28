277e9925711b693271e01ce6074c9e616874c726
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-06-28 17:19:21 +0100

    set up Folder and File classes

--- src/main/java/com/files_and_folders/files_and_folders/models/Folder.java
+++ src/main/java/com/files_and_folders/files_and_folders/models/Folder.java
@@ -1,4 +1,68 @@
 package com.files_and_folders.files_and_folders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "titles")
+    private String title;
+
+    @JsonIgnoreProperties({"folder"})
+    @OneToMany(mappedBy = "folder")
+    private List<File> files;
+    @ManyToOne
+    @JoinColumn(name = "person")
+    @JsonIgnoreProperties({"folders"})
+    private Person person;
+
+    public Folder(String title, Person person) {
+        this.title = title;
+        this.person = person;
+        this.files = new ArrayList<>();
+    }
+
+    // empty for use by Spring
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 }
--- src/main/java/com/files_and_folders/files_and_folders/models/Person.java
+++ src/main/java/com/files_and_folders/files_and_folders/models/Person.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -22,6 +23,7 @@ public class Person {
 
     public Person(String firstName) {
         this.firstName = firstName;
+        this.folders = new ArrayList<>();
     }
     // empty constructor for Spring
     public Person() {
