d48d16bb2e5cc59dda8203b7d35c73df893150d3
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-06-28 16:28:25 +0100

    first commit

---
+++ src/main/java/com/files_and_folders/files_and_folders/FilesAndFoldersApplication.java
@@ -0,0 +1,13 @@
+package com.files_and_folders.files_and_folders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesAndFoldersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesAndFoldersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/files_and_folders/files_and_folders/FilesAndFoldersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.files_and_folders.files_and_folders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesAndFoldersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
